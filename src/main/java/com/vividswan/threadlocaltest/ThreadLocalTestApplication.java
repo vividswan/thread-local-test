e4866ef09348bd3c717563d329c301ff38d231bb
vividswan <vividswan@naver.com>
2024-05-10 23:31:03 +0900

    first init

---
+++ src/main/java/com/vividswan/threadlocaltest/ThreadLocalTestApplication.java
@@ -0,0 +1,13 @@
+package com.vividswan.threadlocaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadLocalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadLocalTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vividswan/threadlocaltest/ThreadLocalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vividswan.threadlocaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadLocalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
