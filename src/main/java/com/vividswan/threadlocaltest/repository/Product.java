fc67010e97d8f6284ea0be4a02b829a263231eae
vividswan <vividswan@naver.com>
2024-05-14 13:37:23 +0900

    feat: Product Entity 생성

---
+++ src/main/java/com/vividswan/threadlocaltest/repository/Product.java
@@ -0,0 +1,17 @@
+package com.vividswan.threadlocaltest.repository;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false, length = 50)
+	private String name;
+	private int stock;
+}
